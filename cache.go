abdb72edf208ade4a343eaeb27de23d14557a0fc
gurkangul <gurkangul05@gmail.com>
2021-06-23 22:53:30 +0300

    dfg

--- cache.go
+++ cache.go
@@ -1,7 +1,9 @@
 package cache
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"sync"
@@ -9,7 +11,8 @@ import (
 )
 
 const (
-	defaultExp = 10
+	defaultExp   = 10
+	WriteTimeout = 1 * time.Second
 )
 
 type Options struct {
@@ -30,10 +33,7 @@ func (s *Store) Set(key string, value string, exp int64) (string, bool) {
 	defer s.mu.Unlock()
 
 	_, isOk := s.kv[key]
-	os.Stderr.WriteString("added: " + fmt.Sprintf("%v", isOk))
-
-	fmt.Println(isOk)
-	if isOk {
+	if isOk == true {
 		return fmt.Sprintf("%s already added", key), false
 	}
 
@@ -56,26 +56,70 @@ func (s *Store) Get(key string) (*Value, bool) {
 	return nil, false
 }
 
+var timeout = time.Duration(2 * time.Second)
+
+func dialTimeout(network, addr string) (net.Conn, error) {
+	return net.DialTimeout(network, addr, timeout)
+}
 func (s *Store) HandleStart() {
 	port := os.Getenv("PORT")
-	http.HandleFunc("/set", func(w http.ResponseWriter, req *http.Request) {
-		keys, ok := req.URL.Query()["key"]
-		if !ok || len(keys[0]) < 1 {
-			fmt.Fprintf(w, "%s", "Url Param 'key' is missing")
+	if port == "" {
+		port = "3030"
+	}
+
+	os.Stderr.WriteString("Listen port : " + port)
+	router := http.NewServeMux()
+	server := &http.Server{
+		Addr:         ":" + port,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: WriteTimeout + 10*time.Millisecond, //10ms Redundant time
+		IdleTimeout:  15 * time.Second,
+	}
+	router.HandleFunc("/set", func(w http.ResponseWriter, req *http.Request) {
+		ctx, _ := context.WithTimeout(context.Background(), WriteTimeout)
+		worker, cancel := context.WithCancel(context.Background())
+		var buffer string
+		go func() {
+			// do something
+			time.Sleep(2 * time.Second)
+			buffer = "ready all response\n"
+			//do another
+			time.Sleep(2 * time.Second)
+			cancel()
+			fmt.Printf("worker finish\n")
+		}()
+		select {
+		case <-ctx.Done():
+			//add more friendly tips
+			w.WriteHeader(http.StatusInternalServerError)
 			return
-		}
-		values, ok := req.URL.Query()["value"]
-		if !ok || len(values[0]) < 1 {
-			fmt.Fprintf(w, "%s", "Url Param 'value' is missing")
+		case <-worker.Done():
+			keys, ok := req.URL.Query()["key"]
+			if !ok || len(keys[0]) < 1 {
+				fmt.Fprintf(w, "%s", "Url Param 'key' is missing")
+				return
+			}
+			values, ok := req.URL.Query()["value"]
+			if !ok || len(values[0]) < 1 {
+				fmt.Fprintf(w, "%s", "Url Param 'value' is missing")
+				return
+			}
+
+			key := keys[0]
+			value := values[0]
+			message, isOk := s.Set(key, value, 20)
+			if isOk == false {
+				fmt.Fprintf(w, message)
+
+			}
+			fmt.Println(buffer)
+			fmt.Fprintf(w, "Key :%s , Value :%s", key, value)
 			return
 		}
 
-		key := keys[0]
-		value := values[0]
-		s.Set(key, value, 20)
-		fmt.Fprintf(w, "Key :%s , Value :%s", key, value)
 	})
-	http.HandleFunc("/get", func(w http.ResponseWriter, req *http.Request) {
+	router.HandleFunc("/get", func(w http.ResponseWriter, req *http.Request) {
 		key, ok := req.URL.Query()["key"]
 
 		if !ok || len(key[0]) < 1 {
@@ -91,16 +135,12 @@ func (s *Store) HandleStart() {
 		fmt.Printf("%#v \n", foundValue)
 		fmt.Printf("%d \n", len(s.kv))
 		fmt.Printf("%#v", s)
-		fmt.Fprintf(w, "%s", foundValue)
+
+		fmt.Fprintf(w, "%s", foundValue.Value)
 
 	})
 
-	if port == "" {
-		port = "3030"
-	}
-
-	os.Stderr.WriteString("Listen port : " + port)
-	http.ListenAndServe(":"+port, nil)
+	server.ListenAndServe()
 }
 
 func (s *Store) CheckExpired() {
