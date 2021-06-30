396109d1b4280a4619849aaf09e67b87548a19c6
gurkangul <gurkangul05@gmail.com>
2021-06-30 23:28:01 +0300

    update

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
-	"os/signal"
 	"sync"
-	"syscall"
 	"time"
 )
 
@@ -15,21 +14,23 @@ const (
 	fileLocation = "./log/"
 	fileMode     = 777
 	defaultExp   = 10
-	WriteTimeout = 3 * time.Second
 )
 
 var (
 	currentPort        = "3030"
 	isLog              = false
-	isHandle           = false
 	defaultWriteSecond = 5
 	timeout            = time.Duration(2 * time.Second)
 )
 
+type Response struct {
+	Message string      `json:"message"`
+	Result  interface{} `json:"result"`
+	Success bool        `json:"success"`
+}
 type Options struct {
 	CheckTime int
 	Port      string
-	IsHandle  bool
 	IsLog     bool
 	WriteTime int
 }
@@ -81,28 +82,36 @@ func (s *store) handleStart() {
 		Addr:         ":" + currentPort,
 		Handler:      router,
 		ReadTimeout:  5 * time.Second,
-		WriteTimeout: WriteTimeout + 10*time.Millisecond, //10ms Redundant time
+		WriteTimeout: timeout + 10*time.Millisecond, //10ms Redundant time
 		IdleTimeout:  15 * time.Second,
 	}
 	router.HandleFunc("/set", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		if req.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			fmt.Fprintf(w, "Method Not Allowed")
 			return
 		}
-		ctx, _ := context.WithTimeout(context.Background(), WriteTimeout)
+		ctx, _ := context.WithTimeout(context.Background(), timeout)
 		worker, cancel := context.WithCancel(context.Background())
 		go func() {
+			resp := &Response{}
 			// do something
 			keys, ok := req.URL.Query()["key"]
 			if !ok || len(keys[0]) < 1 {
-				fmt.Fprintf(w, "%s", "Url Param 'key' is missing")
+				// fmt.Fprintf(w, "%s", "Url Param 'key' is missing")
+				resp.Message = "Url Param 'key' is missing"
+				resp.Success = false
+				json.NewEncoder(w).Encode(resp)
 				cancel()
 				return
 			}
 			values, ok := req.URL.Query()["value"]
 			if !ok || len(values[0]) < 1 {
-				fmt.Fprintf(w, "%s", "Url Param 'value' is missing")
+				// fmt.Fprintf(w, "%s", "Url Param 'value' is missing")
+				resp.Message = "Url Param 'value' is missing"
+				resp.Success = false
+				json.NewEncoder(w).Encode(resp)
 				cancel()
 				return
 			}
@@ -111,11 +120,17 @@ func (s *store) handleStart() {
 			value := values[0]
 			message, isOk := s.set(key, value, 20)
 			if !isOk {
-				fmt.Fprintf(w, "%s", message)
+				// fmt.Fprintf(w, "%s", message)
+				resp.Message = message
+				resp.Success = false
+				json.NewEncoder(w).Encode(resp)
 				cancel()
 				return
 			}
-			fmt.Fprintf(w, "Key :%s , Value :%s", key, value)
+
+			// fmt.Fprintf(w, "Key :%s , Value :%s", key, value)
+			resp = &Response{Message: "success", Result: s.kv[key], Success: true}
+			json.NewEncoder(w).Encode(resp)
 			cancel()
 		}()
 		select {
@@ -129,28 +144,45 @@ func (s *store) handleStart() {
 
 	})
 	router.HandleFunc("/get", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		if req.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			fmt.Fprintf(w, "Method Not Allowed")
 			return
 		}
-		key, ok := req.URL.Query()["key"]
+		ctx, _ := context.WithTimeout(context.Background(), timeout)
+		worker, cancel := context.WithCancel(context.Background())
+		go func() {
+			// do something
+			resp := &Response{}
 
-		if !ok || len(key[0]) < 1 {
-			fmt.Fprintf(w, "%s", "Url Param 'key' is missing")
+			key, ok := req.URL.Query()["key"]
+
+			if !ok || len(key[0]) < 1 {
+				resp.Message = "Url Param 'key' is missing"
+				resp.Success = false
+				json.NewEncoder(w).Encode(resp)
+				return
+			}
+			searchKey := string(key[0])
+			foundValue, _ := s.get(searchKey)
+			if foundValue == nil {
+				resp.Message = "Found nothing"
+				resp.Success = false
+				json.NewEncoder(w).Encode(resp)
+				return
+			}
+			resp = &Response{Message: "success", Result: s.kv[searchKey], Success: true}
+			json.NewEncoder(w).Encode(resp)
+			cancel()
+		}()
+		select {
+		case <-ctx.Done():
+			w.WriteHeader(http.StatusGatewayTimeout)
+			return
+		case <-worker.Done():
 			return
 		}
-		searchKey := string(key[0])
-		foundValue, _ := s.get(searchKey)
-		if foundValue == nil {
-			fmt.Fprintf(w, "%s", "Found nothing")
-			return
-		}
-		fmt.Printf("%#v \n", foundValue)
-		fmt.Printf("%d \n", len(s.kv))
-		fmt.Printf("%#v", s)
-
-		fmt.Fprintf(w, "%s", foundValue.Value)
 
 	})
 
@@ -164,7 +196,6 @@ func (s *store) checkExpired() {
 		select {
 		case t := <-ticker.C:
 			for k, v := range s.kv {
-				fmt.Println(k, v, t)
 				if t.Unix() > v.Expire {
 					delete(s.kv, k)
 				}
@@ -174,10 +205,6 @@ func (s *store) checkExpired() {
 }
 
 func (s *store) Run() {
-	fmt.Println(isHandle, isLog)
-	if isHandle {
-		go s.handleStart()
-	}
 
 	if isLog {
 		go s.writeToFile()
@@ -185,28 +212,27 @@ func (s *store) Run() {
 
 	go s.checkExpired()
 
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// sigs := make(chan os.Signal, 1)
+	// done := make(chan bool, 1)
+	// signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
+	// go func() {
+	// 	sig := <-sigs
+	// 	fmt.Println()
+	// 	fmt.Println(sig)
+	// 	done <- true
+	// }()
 
-	<-done
+	// <-done
+	s.handleStart()
+
 }
 
 func New(opt *Options) *store {
 	if opt.Port != "" {
 		currentPort = opt.Port
 	}
-	if opt.IsHandle != isHandle {
-		isHandle = opt.IsHandle
 
-	}
 	if opt.IsLog != isLog {
 		isLog = opt.IsLog
 	}
@@ -220,14 +246,15 @@ func New(opt *Options) *store {
 }
 
 func (s *store) writeToFile() {
+	timestamp := fmt.Sprintf("%v", time.Now().Unix())
+	logFile, err := os.OpenFile(fileLocation+timestamp+".txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, fileMode)
+
 	ticker := time.NewTicker(time.Second * time.Duration(defaultWriteSecond))
 	defer ticker.Stop()
 	for {
 		select {
 		case t := <-ticker.C:
 			s.mu.Lock()
-			timestamp := fmt.Sprintf("%v", time.Now().Unix())
-			logFile, err := os.OpenFile(fileLocation+timestamp+".txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, fileMode)
 			if err != nil {
 				fmt.Println("opening log file failed")
 				return
@@ -241,16 +268,20 @@ func (s *store) writeToFile() {
 
 			}()
 
-			for k, v := range s.kv {
-				fmt.Println(k, v, t)
-				if !v.Writed {
-					kv := fmt.Sprintf("%s --> %s ", k, v.Value)
-					_, err = logFile.WriteString(kv + "\n")
-					if err != nil {
-						fmt.Println("writing log failed")
+			if len(s.kv) > 0 {
+				for k, v := range s.kv {
+					fmt.Println(k, v, t)
+					if !v.Writed {
+						kv := fmt.Sprintf("%s --> %s ", k, v.Value)
+						_, err = logFile.WriteString(kv + "\n")
+						if err != nil {
+							fmt.Println("writing log failed")
+						}
+						v.Writed = true
 					}
 				}
 			}
+
 			s.mu.Unlock()
 		}
 	}
--- main_test.go
+++ main_test.go
@@ -1,23 +1,26 @@
 package cache
 
 import (
+	"net/http"
 	"testing"
 )
 
-var cache = New(&Options{CheckTime: 1, IsHandle: true, IsLog: true})
+var (
+	cache = New(&Options{CheckTime: 1, IsLog: false})
+)
 
 func TestSet(t *testing.T) {
-	msg, err := cache.set("gg", "bbb", 10)
+	resp, err := http.Get("http://localhost:3030/set?key=foo&value=bar")
 
-	if !err {
-		t.Errorf("%s", msg)
+	if err != nil {
+		t.Errorf("%v", resp)
 	}
 }
 
 func TestGet(t *testing.T) {
-	value, err := cache.get("gg")
+	_, err := http.Get("http://localhost:3030/get?key=foo")
 
-	if !err {
-		t.Errorf("No record %v", value)
+	if err != nil {
+		t.Errorf("No record %v", "foo")
 	}
 }
